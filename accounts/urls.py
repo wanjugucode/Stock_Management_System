f0074e4c98c48e3e3e0d9597d9c4210c206219ca
wanjugucode <karobiaanastasia.com>
2021-11-17 19:22:27 +0300

    login default

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     
    path('register/', views.registerPage, name="register"),
-   path('login/', views.loginPage, name="login"),
+   path('', views.loginPage, name="login"),
 	path('logout/', views.logoutUser, name="logout"),
 
     # path('products/', views.products, name='products'),
