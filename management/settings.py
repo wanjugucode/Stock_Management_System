9dc7913e081d1bad1df2d8f816d26c3d134df079
wanjugucode <karobiaanastasia>
2023-11-20 00:10:57 +0300

    changed db

--- management/settings.py
+++ management/settings.py
@@ -84,23 +84,23 @@ WSGI_APPLICATION = 'management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-#  }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'xpresskitchendb',
-        'USER': 'myuseradmin',
-        'PASSWORD':'passkitchen2021',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+ }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'xpresskitchendb',
+#         'USER': 'myuseradmin',
+#         'PASSWORD':'passkitchen2021',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
 
 
